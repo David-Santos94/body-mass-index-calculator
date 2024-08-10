d9c3c436416572e24e160d618e440091f7bd58af
David Marlon dos Santos <david.marlon94@gmail.com>
2024-08-10 19:24:27 +0100

    up textos e javascript
    upgrade de textos e programação das funcionalidade

---
+++ main.js
@@ -0,0 +1,121 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let alturaElement = document.querySelector('#bodyHeight');
+    let pesoElement = document.querySelector('#bodyWeight');
+    let sistemaMetrico = document.querySelector('#radioMetric');
+    let sistemaImperial = document.querySelector('#radioImperial');
+    let altura = 0;
+    let peso = 0;
+    let sistema = '';
+    let resultado = '';
+
+    sistemaMetrico.addEventListener('change', function() {
+        if (sistemaMetrico.value == 'on') {
+            sistema = 'metrico';
+            document.getElementById('bodyHeightCMouINCH').innerHTML = 'cm';
+            document.getElementById('bodyWeightKGouLB').innerHTML = 'kg';
+            if (alturaElement.value > 0 && pesoElement.value > 0) {
+                alturaElement.value *= 2.54;
+                pesoElement.value /= 2.205;
+            }
+        }
+    })
+    sistemaImperial.addEventListener('change', function() {
+        if (sistemaImperial.value == 'on') {
+            sistema = 'imperial';
+            document.getElementById('bodyHeightCMouINCH').innerHTML = 'in';
+            document.getElementById('bodyWeightKGouLB').innerHTML = 'lb';
+            if (alturaElement.value > 0 && pesoElement.value > 0) {
+                alturaElement.value /= 2.54;
+                pesoElement.value *= 2.205;
+            }
+        }
+    })
+
+    alturaElement.addEventListener("keypress", function(e) {
+        
+        if (sistema == 'metrico') {
+            if(e.key === "," || e.key === ".") {
+                e.preventDefault();
+            }
+        }
+    });
+
+    pesoElement.addEventListener("keypress", function(e) {
+        
+        if (sistema == 'metrico') {
+            if(e.key === "," || e.key === ".") {
+                e.preventDefault();
+            }
+        }
+    });
+    
+    alturaElement.addEventListener('change', function() {
+        altura = alturaElement.value;
+        if (altura > 0 && peso > 0) {
+            resultado = calcularIMC(altura, peso, sistema);
+            exibeResultado(resultado);
+        }
+    })
+    pesoElement.addEventListener('change', function() {
+        peso = pesoElement.value;
+        if (altura > 0 && peso > 0) {
+            resultado = calcularIMC(altura, peso, sistema);
+            exibeResultado(resultado);
+        }
+    })
+
+
+    function calcularIMC(_altura, _peso, _sistema) {
+        let imc = 0;
+        if (_sistema === 'metrico') {
+            _altura /= 100;
+            imc = _peso / (_altura * _altura);
+        } else if (_sistema === 'imperial') {
+            imc = (_peso / (_altura * _altura)) * 703;
+        } else {
+            alert("Sistema inválido. Escolha 'métrico' ou 'imperial'.");
+            alturaElement.value = '';
+            pesoElement.value = '';
+            altura = 0;
+            peso = 0;
+            throw new Error("Sistema inválido. Escolha 'métrico' ou 'imperial'.");
+        }
+    
+        let classificacao, faixaSaudavel;
+        
+        if (imc < 18.5) {
+            classificacao = 'Abaixo do peso';
+            faixaSaudavel = 'IMC saudável está entre 18,5 e 24,9.';
+        } else if (imc >= 18.5 && imc < 24.9) {
+            classificacao = 'Peso normal';
+            faixaSaudavel = 'Mantenha-se dentro dessa faixa!';
+        } else if (imc >= 25 && imc < 29.9) {
+            classificacao = 'Sobrepeso';
+            faixaSaudavel = 'IMC saudável está entre 18,5 e 24,9.';
+        } else if (imc >= 30 && imc < 34.9) {
+            classificacao = 'Obesidade grau 1';
+            faixaSaudavel = 'IMC saudável está entre 18,5 e 24,9.';
+        } else if (imc >= 35 && imc < 39.9) {
+            classificacao = 'Obesidade grau 2';
+            faixaSaudavel = 'IMC saudável está entre 18,5 e 24,9.';
+        } else {
+            classificacao = 'Obesidade grau 3';
+            faixaSaudavel = 'IMC saudável está entre 18,5 e 24,9.';
+        }
+    
+        return {
+            imc: imc.toFixed(2),
+            classificacao: classificacao,
+            faixaSaudavel: faixaSaudavel
+        };
+    }
+
+    function exibeResultado(resultado) {
+        document.getElementById('yourBMI').innerHTML = 'Your BMI is...';
+        document.getElementById('BMI').innerHTML = resultado.imc;
+        document.getElementById('suggests').innerHTML = `Classificação: ${resultado.classificacao}<br>Faixa saudável: ${resultado.faixaSaudavel}`;
+        console.log(`IMC: ${resultado.imc}`);
+        console.log(`Classificação: ${resultado.classificacao}`);
+        console.log(`Faixa saudável: ${resultado.faixaSaudavel}`);
+    }
+})
